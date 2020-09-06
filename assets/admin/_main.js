6e3ac23f5b9e39afff4a25e414392217a1ed1220
Alex Dashkin <alex.dashkin@gmail.com>
2020-09-06 17:36:14 +0300

    Improve addRemove()

--- assets/admin/_main.js
+++ assets/admin/_main.js
@@ -1,4 +1,5 @@
 import 'select2';
+import Handlebars from 'handlebars/dist/handlebars.min.js';
 import Logger from './_logger';
 import Ajax from './_ajax';
 
@@ -143,49 +144,53 @@ export default class {
 		$wrap.trigger('adwpfw.tab_switched', index);
 	}
 
-	addRemove(options, Handlebars) {
-		const _this = this;
-		const $wrap = $(options.wrap);
+	addRemove(opts) {
+		['add', 'remove'].forEach(function (action) {
+			$(opts.wrap).on('click', opts[action], function (e) {
+				e.preventDefault();
 
-		$wrap.on('click', options.add, function (e) {
-			e.preventDefault();
+				const $button = $(this);
+				const $container = $button.closest(opts.container);
+				const $itemsContainer = $container.find('> ' + opts.items);
+				const $items = $itemsContainer.find('> ' + opts.removable);
 
-			const $this = $(this);
-			const $wrapper = $this.closest(options.wrap);
-			const $container = $this.siblings(options.container);
-			const id = $container.find('> ' + options.removable).length;
+				switch (action) {
+					case 'add':
+						const $tpl = $($button.data('tpl'));
+						const template = Handlebars.compile($tpl.html());
+						const $parentRemovable = $container.closest(opts.removable);
+						const id = $items.length ? +$items.last().data('index') + 1 : 0;
+						const newItem = template({
+							id,
+							parentId: $parentRemovable.data('index'),
+						});
 
-			let parentId = 0;
-			const $parentContainer = $container.parents(options.container).first();
+						$(newItem).data('index', id).appendTo($itemsContainer);
 
-			if ($parentContainer.length) {
-				const $selfRemovable = $container.closest(options.removable);
-				parentId = $parentContainer.find('> ' + options.removable).index($selfRemovable);
-			}
+						break;
 
-			const template = Handlebars.compile($wrapper.find($this.data('tpl')).html());
+					case 'remove':
+						$button.closest(opts.removable).remove();
 
-			$container.append(template({id, parentId}));
+						break;
+				}
 
-			_this.reIndex(options);
+				// If data-regex is provided - re-index items by the regex
+				const reIndexRegex = $itemsContainer.data('regex');
 
-			if (options.callback) {
-				options.callback($this);
-			}
-		});
+				if (reIndexRegex) {
+					$itemsContainer.find('> ' + opts.removable).each(function (index) {
+						$(this).data('index', index).find('[name]').each(function () {
+							const $this = $(this);
+							$this.attr('name', $this.attr('name').replace(new RegExp(reIndexRegex), '$1' + index));
+						});
+					});
+				}
 
-		$wrap.on('click', options.remove, function (e) {
-			e.preventDefault();
-			$(this).closest(options.removable).remove();
-			_this.reIndex(options);
-		});
-	}
-
-	reIndex(options) {
-		$(options.container).find(options.removable).each(function (index) {
-			$(this).find('[name]').each(function () {
-				const $this = $(this);
-				$this.attr('name', $this.attr('name').replace(/\[\d*\]/, '[' + index + ']'));
+				// If callback is provided - call it
+				if (opts.callback) {
+					opts.callback($button);
+				}
 			});
 		});
 	}
